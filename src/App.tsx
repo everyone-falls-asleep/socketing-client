29df8a8e1a4544d241e1933c8014e02a04dba002
DongHyun Chae <danny001006@naver.com>
2024-11-11 19:19:16 +0900

    Fix: App.tsx change
    unnecessary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import Button from "./components/atoms/buttons/Button";
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button children="클릭"></Button>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
