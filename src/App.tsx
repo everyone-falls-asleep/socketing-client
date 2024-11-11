01279de6976c61fc1bd6914be1d4a9da0ad2ad3d
hyeda <skdltm622@gmail.com>
2024-11-11 17:10:53 +0900

    feat: Add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Button from "./components/atoms/buttons/Button";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button children="클릭"></Button>
+    </div>
+  );
 }
 
 export default App;
