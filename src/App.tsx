05e16ccec70ee14fb8896b29e974e4d2a393199c
yeonupark <idepix@naver.com>
2024-11-16 04:21:05 +0900

    feat: Save token and user-id using context-api and session-storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import Router from "./shared/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from "./store/UserContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <UserProvider>
+        <Router />
+      </UserProvider>
     </QueryClientProvider>
   );
 }
--- src/components/organisms/Form/LoginForm.tsx
+++ src/components/organisms/Form/LoginForm.tsx
@@ -4,6 +4,7 @@ import Button from "../../atoms/buttons/Button";
 import LabeledInput from "../../molecules/labeledinput/LabeledInput";
 
 import { sendLoginRequest } from "../../../api/authentication/authApi";
+import { getUserInfo } from "../../../api/users/usersApi";
 import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { LoginData, LoginResponse } from "../../../types/api/user";
@@ -12,8 +13,13 @@ import { AxiosError } from "axios";
 import { loginErrorMessages } from "../../../constants/api";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useContext } from "react";
+import { UserContext } from "../../../store/UserContext";
+import { jwtDecode } from "jwt-decode";
 
 const LoginForm = () => {
+  const { setUserId } = useContext(UserContext);
+
   const {
     register,
     handleSubmit,
@@ -30,8 +36,10 @@ const LoginForm = () => {
     mutationFn: sendLoginRequest,
 
     onSuccess: (response: LoginResponse) => {
-      toast.success(`환영합니다 !`);
-      console.log(response);
+      const token = response.data?.accessToken;
+      if (token) {
+        saveAuthInfo(token);
+      }
     },
 
     onError: (error: AxiosError<ApiErrorResponse>) => {
@@ -58,6 +66,33 @@ const LoginForm = () => {
       }
     },
   });
+
+  const saveAuthInfo = (token: string) => {
+    sessionStorage.setItem("authToken", token);
+
+    const decodedToken = jwtDecode(token);
+    const userId = decodedToken.sub;
+
+    if (userId) {
+      setUserId(userId);
+      void fetchUserInfo(userId);
+    } else {
+      console.error("cannot find userID from token");
+    }
+  };
+
+  const fetchUserInfo = async (user_id: string) => {
+    try {
+      const data = await getUserInfo(user_id);
+      const nickname = data.data?.nickname;
+      if (nickname) {
+        sessionStorage.setItem("nickname", nickname);
+        toast.success(`안녕하세요, ${nickname}님!`);
+      }
+    } catch (error) {
+      console.error("사용자 정보 불러오기 실패:", error);
+    }
+  };
 
   const onSubmit = (data: LoginData) => {
     mutation.mutate(data);
