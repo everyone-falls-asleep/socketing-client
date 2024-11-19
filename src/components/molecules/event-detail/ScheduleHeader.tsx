ab92e43b202c7b178264aaa4f879a55ba325a642
jee-in <chris309804@gmail.com>
2024-11-19 23:18:10 +0900

    feat: Update event detail page for presentation

--- src/components/molecules/event-detail/ScheduleHeader.tsx
+++ src/components/molecules/event-detail/ScheduleHeader.tsx
@@ -11,43 +11,50 @@ const ScheduleHeader = ({
   selectedDates,
   onDateSelect,
 }: ScheduleHeaderProps) => {
-  const renderSelectedDates = () => {
-    return selectedDates.length > 0 ? (
-      selectedDates.map((date) => (
-        <div key={date.getTime()} className="selected-date-label">
-          <span className="text-gray-700 text-sm">
-            {date.toLocaleDateString()}
-          </span>
-          <button
-            className="label-remove-icon"
-            onClick={() =>
-              onDateSelect(
-                selectedDates.filter((d) => d.getTime() !== date.getTime())
-              )
-            }
-          >
-            ×
-          </button>
-        </div>
-      ))
-    ) : (
-      <span className="text-gray-600 text-sm">날짜를 선택해주세요</span>
-    );
-  };
+  // const renderSelectedDates = () => {
+  //   return selectedDates.length > 0 ? (
+  //     selectedDates.map((date) => (
+  //       <div key={date.getTime()} className="selected-date-label">
+  //         <span className="text-gray-700 text-sm">
+  //           {date.toLocaleDateString()}
+  //         </span>
+  //         <button
+  //           className="label-remove-icon"
+  //           onClick={() =>
+  //             onDateSelect(
+  //               selectedDates.filter((d) => d.getTime() !== date.getTime())
+  //             )
+  //           }
+  //         >
+  //           ×
+  //         </button>
+  //       </div>
+  //     ))
+  //   ) : (
+  //     <span className="text-gray-600 text-sm">날짜를 선택해주세요</span>
+  //   );
+  // };
 
   return (
-    <div className="border border-gray-300 mt-2 mb-4 rounded-sm bg-gray-100 w-full">
-      <div className="flex items-center p-4">
+    <div id="schedule-header" className="mb-4 rounded-sm bg-rose-100 w-full">
+      {/* <div
+        id="shcedule-header-label-container"
+        className="flex items-center p-4"
+      >
         <div className="flex flex-wrap items-center ml-4">
           {renderSelectedDates()}
         </div>
+      </div> */}
+      <div
+        id="schedule-calendar-container"
+        className="w-full p-2 flex justify-center"
+      >
+        <ScheduleCalendar
+          validDates={validDates}
+          selectedDates={selectedDates}
+          onDateSelect={onDateSelect}
+        />
       </div>
-
-      <ScheduleCalendar
-        validDates={validDates}
-        selectedDates={selectedDates}
-        onDateSelect={onDateSelect}
-      />
     </div>
   );
 };
