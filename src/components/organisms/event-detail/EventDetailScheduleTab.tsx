5b36984141d8b2b4bc95cfbb599fcda67539a8b2
최지인 <chris309804@gmail.com>
2024-11-25 17:16:27 +0900

    feat: Add registering friend button in event detail page

--- src/components/organisms/event-detail/EventDetailScheduleTab.tsx
+++ src/components/organisms/event-detail/EventDetailScheduleTab.tsx
@@ -4,12 +4,19 @@ import { useEventDetail } from "../../../store/EventDetailContext";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import ModalWithForm from "../../molecules/modal-with-form/ModalWithForm";
+import { User } from "../../../types/api/user";
+import { useState } from "react";
+import Button from "../../atoms/buttons/Button";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const EventDetailScheduleTab = () => {
   const { filteredEvent, selectedDates, setSelectedDates } = useEventDetail();
+  const { friends } = useEventDetail();
+  const [isFriendRegisterModalOpen, setIsFriendRegisterModalOpen] =
+    useState(false);
 
   if (!filteredEvent) {
     return null;
@@ -38,12 +45,33 @@ const EventDetailScheduleTab = () => {
           />
         </div>
         <div className="w-[50%] pt-5">
+          <Button
+            variant="primary"
+            onClick={() => setIsFriendRegisterModalOpen(true)}
+          >
+            연석 친구 등록하기 ({friends.length}명)
+          </Button>
           <ScheduleList
             filteredEvent={filteredEvent}
             selectedDates={selectedDates}
           />
         </div>
       </div>
+      <ModalWithForm<User>
+        isOpen={isFriendRegisterModalOpen}
+        onClose={() => setIsFriendRegisterModalOpen(false)}
+        onSuccess={() => {}}
+        formTitle="연석 친구 등록하기"
+        fields={[
+          {
+            name: "email",
+            label: "이메일",
+            validation: { required: "이메일을 입력해 주세요" },
+          },
+        ]}
+        submitButtonText="친구 추가하기"
+        cancelButtonText="취소"
+      />
     </>
   );
 };
