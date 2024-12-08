814181282344e15742c61ae2dfe6595cd6a06f67
hyeda <skdltm622@gmail.com>
2024-12-09 00:48:04 +0900

    feat: Update payment-page

--- src/components/pages/OrderPage.tsx
+++ src/components/pages/OrderPage.tsx
@@ -109,8 +109,8 @@ const OrderPage = () => {
     }
   };
   if (!orderData) return;
-  const order = orderData.order;
-  if (!order) return;
+  // const order = orderData.order;
+  // if (!order) return;
   const seats = orderData.seats;
 
   return (
@@ -163,12 +163,12 @@ const OrderPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md">
                 <h2 className="text-lg font-bold mb-4">주문자 정보</h2>
                 <div className="flex justify-between items-center">
-                  <div className="space-y-1">
+                  {/* <div className="space-y-1">
                     <p className="text-sm text-gray-600">
                       {order.user.nickname}
                     </p>
                     <p className="text-sm text-gray-600">{order.user.email}</p>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
